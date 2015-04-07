bb90eb8c2fa6fe21d8ce2034ff329e35308787c2
li25a <li25a@mtholyoke.edu>
2015-04-07 11:15:24 -0400

    updated linkDB.js file with connection

--- linkDB.js
+++ linkDB.js
@@ -1,19 +1,39 @@
-// Require the express library:
+// Module dependencies
 var express = require('express');
- 
-// Require our database library:
-var db = require('db');
- 
+var mysql = require('mysql');
+var hbs = require('hbs');
+var path = require('path');
 
 //Require URL library
 var url = require('url');
 var url_parts = url.parse(request.url, true);
 
+//var http = require('http');
+
+//load objects route
+var objects = require('./routes/objects');
+
+//create an app
+var app = express();
+
+// Configure MySQL parameters.
+var connection = mysql.createConnection({
+  host: 'localhost',
+  user: 'root',
+  password: 'marylyon',
+  database: 'meadmuseum'
+});
+
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'html');
+app.engine('html', hbs.__express);
+
+
+app.use(express.static(path.join(__dirname, 'public')));
+
 //store URL query
 var query = url_parts.query;
 
-// Create an app:
-var app = express();
 
 //to test
 console.log("query: " + url.format(query)); 
@@ -21,6 +41,40 @@ console.log("req query" + req.query('id'));
 console.log ("query.id" + query.id);
 console.log ("location" + location);
 
+// Connect to database.
+connection.connect(function(err){
+  if(err){
+    console.log("There was a problem connecting to the database: " + err);
+  }
+  else{
+    console.log("The connection to the database was sucessful.");
+  }
+});
+
+// Query the database.
+app.get('/objects', function(req, res){
+   var urlID = query.id;
+  connection.query( 'select * from objects where id=' + urlID, function(err, result, fields) {
+    if (err) throw err;
+    res.render('objects', {page_title:"Objects", objects:result});
+     console.log ("error, cannot locate object");
+    // End the connection.
+    connection.end();
+  });
+});
+
+app.get('/', function(req, res) {
+  res.sendfile('./views/index.html');
+});
+
+/*app.get('/objects', function(req, res) {
+  res.render('objects', {title: "Objects", objects:objects.getObjects()});
+});*/
+
+var server = app.listen(3000, function () {
+  console.log('Listening on port %d', server.address().port);
+});
+/*
 // A route to list all users and provide a form to add more.
 app.get('/users', function (req, res) {
 //display list of users
@@ -45,17 +99,11 @@ app.get('/users', function (req, res) {
 
 
 function getObjFromUrl (query){
-//pull title from url
- // var titleUrl = query.parse(...);
-
   //querystring.parse?
 
 //pull from database
 //check syntax
   var obj = 'select * from objects where id=' + query.id;
   return obj;}
- 
-//start the server
-  var server = app.listen(3000, function () {
-          console.log('Listening on port %d', server.address().port);
+
 })
