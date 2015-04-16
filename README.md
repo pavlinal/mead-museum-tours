e8f2fdd9038ca3875c502469abc6dc83de651391
li25a <li25a@mtholyoke.edu>
2015-04-16 11:12:12 -0400

    same struc

---
+++ README.md
@@ -0,0 +1,17 @@
+### Mead Art Museum
+
+#### Instructions:  
+To run the application, first make sure to install the necessary modules. To do that cd into the *mead-museum-tours* folder in your terminal and then execute the following command:
+	
+	npm install
+
+Once the modules are installed, you should be able to run the application by typing:
+
+	npm start
+	> mmtours@0.0.1 start /vagrant/mead-museum-tours
+	> node ./bin/www
+
+If this command does not work, you can also use:
+
+	node ./bin/www
+
---
+++ app.js
@@ -0,0 +1,33 @@
+var express = require('express');
+
+// Regular express built-in middleware:
+var path = require('path');
+
+// Routes:
+var objects = require('./routes/objects');
+var main = require('./routes/main');
+
+// Create the express application:
+var app = express();
+
+// Setup the view engine:
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Using routes:
+app.use('/objects', objects);
+app.use('/', main);
+
+app.get('/', function (req, res) {
+  res.redirect('/index');
+});
+
+// Export the app as the module:
+module.exports = app;
+
+/*var server = app.listen(3000, function () {
+  console.log('Listening on port %d', server.address().port);
+});*/
+
