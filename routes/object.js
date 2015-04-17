8e705f29ce910c1089c4d6f0edb1704b66b3e0bf
li25a <li25a@mtholyoke.edu>
2015-04-17 01:41:45 -0400

    preliminary vers of object.js

--- routes/object.js
+++ routes/object.js
@@ -3,14 +3,18 @@ var router = express.Router();
 
 var dblib = require('../lib/db');
 
+var url = require('url');
+var url_parts = url.parse(request.url, true);
+
 /*GET objects*/
 router.get('/list', function(req, res) {
-  var objects = dblib.getObjects(function(error, objects) {
+  var objects = dblib.getObject(function(error, objects) {
     if (error) {
       //req.flash('', error);
       //res.redirect('/');
     } else {
-      res.render('list', {title:'List of Objects', objects:objects});
+    	//not sure..???...
+      res.render('Object', {title: object.title , object: object});
     }
   }); 
 });
